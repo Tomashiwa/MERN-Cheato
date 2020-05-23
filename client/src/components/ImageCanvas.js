864cff3af68517c453c5ac7d44f81d2ff06e4ae9
Tomashiwa <wongqinliang@gmail.com>
2020-05-23 18:05:49 +0800

    Clear canvas upon new submission

--- client/src/components/ImageCanvas.js
+++ client/src/components/ImageCanvas.js
@@ -1,23 +1,24 @@
 import React, { useState, useEffect, useRef, useContext } from 'react';
-import { ImagesContext, ConfigContext } from "../App";
+import { ImagesContext } from "../App";
 
 import "./css/ImageCanvas.css";
 
 function ImageCanvas() {
     const [width, setWidth] = useState(1122);
     const [height, setHeight] = useState(794);
-    
+
     const imagesContext = useContext(ImagesContext);
     const canvasRef = useRef(null);
 
     useEffect(() => {
         const context = canvasRef.current.getContext("2d");
+        context.clearRect(0, 0, width, height);
         imagesContext.images.forEach(i => context.drawImage(i.element, i.x, i.y));
-    }, [imagesContext.images])
+    }, [imagesContext.images, width, height])
 
     return (
         <div>
-            <canvas class="canvas-img" ref={canvasRef} width={width} height={height}></canvas>        
+            <canvas className="canvas-img" ref={canvasRef} width={width} height={height}></canvas>        
         </div>
     )
 }
