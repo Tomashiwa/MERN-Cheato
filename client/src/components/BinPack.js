6b0f1f4c8ca3832e2090d1b865b01bf7cc69b4b8
Jacob-109 <65383310+Jacob-109@users.noreply.github.com>
2020-05-26 11:11:35 +0800

    CanvasSize update

--- client/src/components/BinPack.js
+++ client/src/components/BinPack.js
@@ -2,26 +2,25 @@
 import GrowingPacker from "../library/GrowingPacker"
 import Packer from "../library/Packer"
 import { ImagesContext, ConfigContext } from "../App"
-import React, { useEffect,useContext,componentDidMount } from 'react'
+import React, { useEffect,useContext,props } from 'react'
 
 
-function BinPack() {
+function BinPack({setWidth,setHeight}) {
   const value = useContext(ConfigContext);
   const uploaded = useContext(ImagesContext);
   
+  
   useEffect(()=> {
-    const Arrange = document.getElementById("input-btn-arr");
-    Arrange.addEventListener("click",run);
-    return () => Arrange.removeEventListener("click",run);
-});
+    run();
+    //const Arrange = document.getElementById("input-btn-arr");
+    //Arrange.addEventListener("click",run);
+    //return () => Arrange.removeEventListener("click",run);
+  }, [uploaded.images]);
   
-  
-  function run()  {
+  function run() {
       var packer = new GrowingPacker();   
       var blocks = uploaded.images;
-      console.log(uploaded)
-      console.log(uploaded.images)
-      //console.log(uploaded.images[0].height)
+  
       var num = 0;
       
       function counter() {
@@ -29,42 +28,103 @@ function BinPack() {
         return num;
       }
       
-      const blockade =blocks.map(img => {
+      const blockade = blocks.map(img => {
         return {
             id: counter(),
             w: img.width, 
             h: img.height      
         };
       });
-      console.log(blocks[0])
-      console.log(blockade[0])
-
+      
       function msort(array,type) {
-        if (type == "largestSide") {
+        if (type === "largestSide") {
           array.sort(function(a,b) { return Math.max(b.w, b.h) - Math.max(a.w, a.h); });
-        } else if (type == "area") {
+        } else if (type === "area") {
           array.sort(function(a,b) { return (b.w * b.h) - (a.w * a.h); });
-        } else if (type == "width") {
+        } else if (type === "width") {
           array.sort(function(a,b) { return b.w - a.w; });
-        } else if (type == "height") {
+        } else if (type === "height") {
           array.sort(function(a,b) { return b.h - a.h; });
         }
       }
     
-    console.log(value.config.sortOrder)
     msort(blockade,value.config.sortOrder);
     
     packer.fit(blockade);
     
     blockade.sort(function(a,b) { return a.id - b.id; });
     
-    for(var i = 0;i < uploaded.images.length;i++) {
+    
+    
+    for(var i = 0;i < blockade.length;i++) {
+      if (blockade[i].fit) {
       blocks[i].x = blockade[i].fit.x;
       blocks[i].y =blockade[i].fit.y;
+      }
     }
+  
+  var sortByX = null;  
+  var sortByY = null;
+  
+  function findLargestWidth(array) {
+    sortByX = array.sort(function(a,b) { return (b.x + b.width) - (a.x + a.width) });
+    if(sortByX.length !== 0) {
+      return sortByX[0].x + sortByX[0].width;
+    } else {
+      return null;
+    }
+  }
+  
+  function findLargestHeight(array) {
+    sortByY = array.sort(function(a,b) { return (b.y + b.height) - (a.y + a.height) });
+    if(sortByY.length !== 0) {
+      return sortByY[0].y + sortByY[0].height;
+    } else {
+      return null;
+    }
+  }
    
-   
-    /* const final =blockade.map(block => {
+  var x = 0;
+  var y = 0;
+  
+  function findMax(width,height) {
+    if(width > height) {
+      x = width;
+      y = width * (794/1122);
+       } else if (height > width) {
+      x = height * (1122/794);
+      y = height;
+    }
+    return [x,y];
+  }
+
+  const canvasWidth = findMax(findLargestWidth(blocks),findLargestHeight(blocks))[0];
+  const canvasLength = findMax(findLargestWidth(blocks),findLargestHeight(blocks))[1];
+
+
+  function setCanvasSize(width,height) {
+    if(width < 1122 && height < 794) {
+      setWidth(1122);
+      setHeight(794);
+    } else {
+      setWidth(width);
+      setHeight(height);
+    }
+  }  
+
+  setCanvasSize(canvasWidth,canvasLength);
+  
+ /* function filterDrawing(width,height,array) {
+    for(var i = 0;i < array.length;i++) {
+      if(array[i].x > width && array[i].y > height) {
+        array[i].isRejected = true;
+      }
+    }
+  }
+  
+  filterDrawing(canvasWidth,canvasLength,blocks);
+  */
+  /* const final =blockade.map(block => {
       return {
           w: block.w, 
           h: block.h, 
@@ -91,6 +151,5 @@ function BinPack() {
   
   return (<div> </div>);
 }
-
 
 export default BinPack;
