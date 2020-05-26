68d52fc2fe77a843adfb4aacf3f8cc063589ab83
Tomashiwa <wongqinliang@gmail.com>
2020-05-26 13:20:37 +0800

    Downloading of scaled down image from ImageCanvas

--- client/src/components/ImageCanvas.js
+++ client/src/components/ImageCanvas.js
@@ -1,15 +1,13 @@
 import React, { useState, useEffect, useRef, useContext } from 'react';
 import BinPack from "../components/BinPack"
 import { ImagesContext } from "../App";
+import { Button } from "reactstrap";
 
 import "./css/ImageCanvas.css";
 
 function ImageCanvas() {
-    // const width = 1122 * 2;
-    // const height = 794 * 2;
     const [width, setWidth] = useState(1122);
     const [height, setHeight] = useState(794);
-
     const [isDragging, setIsDragging] = useState(false);
     const [clickedImage, setClickedImage] = useState(null);
     const [clickOffset, setClickOffset] = useState({x: 0, y: 0});
@@ -32,19 +30,11 @@ function ImageCanvas() {
             const canvasBound = canvasRef.current.getBoundingClientRect();
             const scaleFactor = canvasBound.width / canvasRef.current.width;
             const clickPos = {x: e.clientX - canvasBound.x, y: e.clientY - canvasBound.y};
-            
-            // console.log(`Clicked @ ${clickPos.x}, ${clickPos.y}`);
-            // console.log(`scaleFactor: ${scaleFactor}`);
 
             const clickedImages = imagesContext.images.slice().reverse().filter(image => {
-                // console.log(`image.x: ${scaleFactor * image.x} ~ ${scaleFactor * (image.x + image.width)}`);
-                // console.log(`image.y: ${scaleFactor * image.y} ~ ${scaleFactor * (image.y + image.height)}`);
-                
                 return clickPos.x >= (scaleFactor * image.x) && clickPos.x <= (scaleFactor * (image.x + image.width)) &&
                        clickPos.y >= (scaleFactor * image.y) && clickPos.y <= (scaleFactor * (image.y + image.height));
             })
-
-            // console.log(`Clicked images: ${clickedImages.length}`);
 
             if(clickedImages.length > 0) {
                 setIsDragging(true);
@@ -72,13 +62,6 @@ function ImageCanvas() {
                 imagesContext.images
                     .filter(i => !i.isRejected)
                     .forEach(i => context.drawImage(i.element, i.x, i.y));
-
-                // console.log(`scaleFactor: ${scaleFactor}`);
-                // console.log(`clickPos (pre): ${clickPos.x}, ${clickPos.y}`);
-                // console.log(`clickPos (post): ${scaleFactor * clickPos.x}, ${scaleFactor * clickPos.y}`);
-                // console.log(`clickOffset (pre): ${clickOffset.x}, ${clickOffset.y}`);
-                // console.log(`clickOffset (post): ${scaleFactor * clickOffset.x}, ${scaleFactor * clickOffset.y}`);
-                // console.log(`clickedImage: ${clickedImage.x}, ${clickedImage.y}`);
             }
         }
 
@@ -95,11 +78,37 @@ function ImageCanvas() {
             currentCanvas.removeEventListener("mouseout", handleMouseUp);
         }
     }, [imagesContext.images, isDragging, clickedImage, width, height, clickOffset])
- 
+
+    //Download a scaled down image of the canvas 
+    useEffect(() => {
+        const downloadBtn = document.querySelector("#canvas-btn-download");
+        const download = () => {
+            console.log("Download");
+
+            const resizedCanvas = document.createElement("canvas");
+            const resizedContext = resizedCanvas.getContext("2d");
+            resizedCanvas.width = "1122";
+            resizedCanvas.height = "794";
+
+            resizedContext.drawImage(canvasRef.current, 0, 0, 1122, 794);
+            
+            const a = document.createElement("a");
+            document.body.appendChild(a);
+            a.href = resizedCanvas.toDataURL();
+            a.download = "cheatsheet.jpg";
+            a.click();
+            document.body.removeChild(a);
+        };
+
+        downloadBtn.addEventListener("click", download);
+        return () => downloadBtn.removeEventListener("click",download);
+    }, [])
+
     return (
         <div>
             <BinPack setWidth={setWidth} setHeight={setHeight}/>
-            <canvas className="canvas-img" ref={canvasRef} width={width} height={height}></canvas>        
+            <canvas className="canvas-img" ref={canvasRef} width={width} height={height} />
+            <Button id="canvas-btn-download" color="dark">Download</Button>
         </div>
     )
 }
