0d37c1a7b0990c8410215658ed88705bbdd8c3eb
Tomashiwa <wongqinliang@gmail.com>
2020-06-07 14:47:17 +0800

    Step navigation for Create page / Storing Create form data

--- client/src/components/CreateForm.js
+++ client/src/components/CreateForm.js
@@ -1,34 +1,78 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 import {Form, FormGroup, Label, Input, FormText} from "reactstrap";
 
 import "./css/CreateForm.css"
 
-function CreateForm() {
+function CreateForm({form, setForm}) {
+    //Load previous entires if avaliable
+    useEffect(() => {
+        const nameInput = document.querySelector("#createform-input-name");
+        const instInput = document.querySelector("#createform-input-insti");
+        const modInput = document.querySelector("#createform-input-mod");
+        const descInput = document.querySelector("#createform-input-desc");
+        const isPublicInput = document.querySelector("#createform-input-public");
+
+        nameInput.value = form.name;
+        instInput.value = form.institution;
+        modInput.value = form.module;
+        descInput.value = form.description;
+        isPublicInput.checked = form.isPublic;
+    }, [form]);
+
+    //Save given details
+    useEffect(() => {
+        const nameInput = document.querySelector("#createform-input-name");
+        const instInput = document.querySelector("#createform-input-insti");
+        const modInput = document.querySelector("#createform-input-mod");
+        const descInput = document.querySelector("#createform-input-desc");
+        const isPublicInput = document.querySelector("#createform-input-public");
+
+        const changeName = e => setForm({...form, ...{name: e.target.value}});
+        const changeInsti = e => setForm({...form, ...{institution: e.target.value}});
+        const changeMod = e => setForm({...form, ...{module: e.target.value}});
+        const changeDesc = e => setForm({...form, ...{description: e.target.value}});
+        const changeIsPublic = e => setForm({...form, ...{isPublic: e.target.checked}});
+
+        nameInput.addEventListener("change", changeName);
+        instInput.addEventListener("change", changeInsti);
+        modInput.addEventListener("change", changeMod);
+        descInput.addEventListener("change", changeDesc);
+        isPublicInput.addEventListener("change", changeIsPublic);
+
+        return () => {
+            nameInput.removeEventListener("change", changeName);
+            instInput.removeEventListener("change", changeInsti);
+            modInput.removeEventListener("change", changeMod);
+            descInput.removeEventListener("change", changeDesc);
+            isPublicInput.removeEventListener("change", changeIsPublic);
+        }
+    }, [form, setForm])
+
     return (
         <Form id="form-create">
             <FormGroup>
                 <Label>Name</Label>
-                <Input />
+                <Input id="createform-input-name"/>
                 <FormText>Enter the name of your cheatsheet.</FormText>
             </FormGroup>
             <FormGroup>
                 <Label>Institution</Label>
-                <Input />
+                <Input id="createform-input-insti"/>
                 <FormText>Institution that your cheatsheet is for.</FormText>
             </FormGroup>
             <FormGroup>
                 <Label>Module</Label>
-                <Input />
+                <Input id="createform-input-mod"/>
                 <FormText>Module that your cheatsheet is for.</FormText>
             </FormGroup>
             <FormGroup>
                 <Label>Description</Label>
-                <Input type="textarea"/>
+                <Input id="createform-input-desc" type="textarea"/>
                 <FormText>Information that may help readers understand your cheatsheet.</FormText>
             </FormGroup>
             <FormGroup check>
                 <Label check>
-                    <Input type="checkbox"/> Share with public
+                    <Input id="createform-input-public" type="checkbox"/> Share with public
                 </Label>
             </FormGroup>
         </Form>
--- client/src/pages/Create.js
+++ client/src/pages/Create.js
@@ -1,16 +1,73 @@
-import React from 'react'
+import React, { useState, useEffect, useRef } from 'react'
 
 import AppNavbar from '../components/AppNavbar'
-import ImageCanvas from "../components/ImageCanvas";
 
-import Container from 'reactstrap/lib/Container';
+import ImageCanvas from "../components/ImageCanvas";
+import CreateForm from '../components/CreateForm';
+import ImagePreviewer from '../components/ImagePreviewer';
+
+import {Container, Button} from 'reactstrap';
+import "./css/Create.css"
+
+export const CREATE_STEP_IMPORT = 1;
+export const CREATE_STEP_FORM = 2;
+export const CREATE_STEP_PREVIEW = 3;
 
 function Create() {
+    const [formStep, setFormStep] = useState(CREATE_STEP_IMPORT);
+    const [form, setForm] = useState({
+        canvas: null,
+        url: "",
+        name: "",
+        institution: "",
+        module: "",
+        description: "",
+        isPublic: false
+    });
+
+    //Form navigation
+    useEffect(() => {
+        const prev = e => {
+            const prevStep = formStep - 1 > 0
+                ? formStep - 1
+                : CREATE_STEP_IMPORT;
+            setFormStep(prevStep);
+        };
+
+        const next = e => {
+            const nextStep = formStep + 1 < 3
+                ? formStep + 1
+                : CREATE_STEP_PREVIEW;
+            setFormStep(nextStep);
+        };
+
+        const prevBtn = document.querySelector("#create-btn-prev");
+        const nextBtn = document.querySelector("#create-btn-next");
+
+        prevBtn.addEventListener("click", prev);
+        nextBtn.addEventListener("click", next);
+
+        return () => {
+            prevBtn.removeEventListener("click", prev);
+            nextBtn.removeEventListener("click", next);
+        }
+    }, [formStep])
+
     return (
         <div>
             <AppNavbar />
-            <Container id="container">
-                <ImageCanvas />
+            <Container id="create-container">
+                <Button id="create-btn-prev">Previous</Button>
+                <Button id="create-btn-next">Next</Button>
+                {
+                    formStep === CREATE_STEP_IMPORT
+                        ? <ImageCanvas setForm={setForm}/>
+                    : formStep === CREATE_STEP_FORM
+                        ? <CreateForm form={form} setForm={setForm} />
+                    : formStep === CREATE_STEP_PREVIEW
+                        ? <ImagePreviewer imageURL={form.url} />
+                    : <div></div>
+                }
             </Container>
         </div>
     )
