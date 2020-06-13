dbcf9c2819bdd864b2abecf879c2f28a13cfd44a
Jacob-109 <65383310+Jacob-109@users.noreply.github.com>
2020-06-13 15:01:07 +0800

    NavBar Changes

--- client/src/components/AppNavbar.js
+++ client/src/components/AppNavbar.js
@@ -33,18 +33,23 @@ function AppNavbar() {
                     <Collapse isOpen={isOpen} navbar>
                         <Nav className="ml-auto" navbar>
                             <NavItem>
-                                <form class = "form-inline">
-                                    <input class = "form-control mr-sm-1" type = "search"
+                                <form className = "form-inline">
+                                    <input className = "form-control mr-sm-1" type = "search"
                                         placeholder = "Search" >
                                     </input>
-                                    <button class = "btn btn-light my-sm-2" type = "submit">
-                                        <svg class="bi bi-search" width="1em" height="1em" viewBox="0 0 16 16" fill="currentColor" xmlns="http://www.w3.org/2000/svg">
-                                            <path fill-rule="evenodd" d="M10.442 10.442a1 1 0 0 1 1.415 0l3.85 3.85a1 1 0 0 1-1.414 1.415l-3.85-3.85a1 1 0 0 1 0-1.415z"/>
-                                            <path fill-rule="evenodd" d="M6.5 12a5.5 5.5 0 1 0 0-11 5.5 5.5 0 0 0 0 11zM13 6.5a6.5 6.5 0 1 1-13 0 6.5 6.5 0 0 1 13 0z"/>
+                                    <button className = "btn btn-light my-sm-2" type = "submit">
+                                        <svg className ="bi bi-search" width="1em" height="1em" viewBox="0 0 16 16" fill="currentColor" xmlns="http://www.w3.org/2000/svg">
+                                            <path fillRule="evenodd" d="M10.442 10.442a1 1 0 0 1 1.415 0l3.85 3.85a1 1 0 0 1-1.414 1.415l-3.85-3.85a1 1 0 0 1 0-1.415z"/>
+                                            <path fillRule="evenodd" d="M6.5 12a5.5 5.5 0 1 0 0-11 5.5 5.5 0 0 0 0 11zM13 6.5a6.5 6.5 0 1 1-13 0 6.5 6.5 0 0 1 13 0z"/>
                                         </svg>
                                     </button>
                                 </form>
+<<<<<<< Updated upstream
                             </NavItem>
+=======
+                             </NavItem>
+                        <div className="d-flex align-items-center">
+>>>>>>> Stashed changes
                             <NavItem>
                                 <NavLink href="/create">
                                     Create
