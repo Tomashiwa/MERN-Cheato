f938d1a035b387b991d2e34d8f060cab861dc6aa
Tomashiwa <wongqinliang@gmail.com>
2020-06-14 20:06:05 +0800

    Autocompletion from Searchbar / Placeholding navigation from Searchbar
    Note: Autocompletion result is still inconsistent and placeholder navigation just jump to random pages for now

--- client/src/components/SearchBar.js
+++ client/src/components/SearchBar.js
@@ -6,38 +6,62 @@ import { useHistory } from "react-router-dom";
 
 function SearchBar() {
     const [searchTerm, setSearchTerm] = useState("");
-    const [cheatsheets, setCheatsheets] = useState([]);
     const [autocompletes, setAutocompletes] = useState([]);
-
     const history = useHistory();
+
+    const resultLimit = 5;
 
     // Fetch similar cheatsheets from backend based on given input
     useEffect(() => {
-        axios.get("/api/cheatsheets")
-            .then(res => {
-                setCheatsheets(res.data);
-                console.log("Fetched cheatsheets");
-            })
-            .catch(err => {
-                console.log(`Fail to fetch cheatsheets: ${err}`);
-            });
-    }, [searchTerm, setCheatsheets])
+        const nameComparator = (a, b) => {
+            return a.name < b.name
+                    ? -1
+                : a.name > b.name
+                    ? 1 : 0;
+        }
 
-    // Convert fetched cheatsheets to potential autocompletes to be suggested to user
-    useEffect(() => {
-        const newAutocompletes = cheatsheets.map(cheatsheet => {
-            return {key: cheatsheet._id, value: cheatsheet.school};
-        });
-        
-        console.log("newAutocompletes:");
-        console.log(newAutocompletes);
-        
-        setAutocompletes(newAutocompletes);
-    }, [cheatsheets])
+        if(searchTerm.length > 0) {
+            const searchSheets = axios.get(`/api/cheatsheets/search/${searchTerm}/${resultLimit}`);
+            const searchSchools = axios.get(`/api/schools/search/${searchTerm}/${resultLimit}`);
+            const searchModules = axios.get(`/api/modules/search/${searchTerm}/${resultLimit}`);
+    
+            Promise
+                .all([searchSheets, searchSchools, searchModules])
+                .then(results => {
+                    const sheets = Array.prototype.slice.call(results[0].data)
+                        .sort(nameComparator)
+                        .map(sheet => {return {key: sheet._id, value: `Sheet - ${sheet.name}`}});
+                    const schools =  Array.prototype.slice.call(results[1].data)
+                        .sort(nameComparator)
+                        .map(school => {return {key: school._id, value: `School - ${school.name}`}});
+                    const modules = Array.prototype.slice.call(results[2].data)
+                        .sort(nameComparator)
+                        .map(module => {return {key: module._id, value: `Module - ${module.name}`}});
+                
+                    const newAutocompletes = sheets.concat(schools, modules).slice(0, resultLimit);
+
+                    console.log("Autocompletes:");
+                    console.log(newAutocompletes);
+
+                    setAutocompletes(newAutocompletes);
+                });
+        }
+    }, [searchTerm]);
 
     // Go to the view page of the cheatsheet selected by user
-    const viewCheatsheet = option => {
-        history.push(`/view/${option.key}`);
+    const browseOption = option => {
+        console.log(`option clicked: ${option.value}`);
+        const clicked = option.value;
+
+        if(clicked.includes("Sheet")) {
+            history.push(`/view/${option.key}`);
+        } else if(clicked.includes("School")) {
+            console.log(`View sheets from the school, ${clicked}`);
+            history.push(`/${option.key}`);
+        } else if(clicked.includes("Module")) {
+            console.log(`View sheets from the module, ${clicked}`);
+            history.push(`/${option.key}`);
+        }
     }
 
     // Update searchTerm based on user's input
@@ -47,11 +71,14 @@ function SearchBar() {
 
     return (
         <ReactSearchBox
-            placeholder="Search for cheatsheets here..."
+            placeholder="Search here..."
             data={autocompletes}
-            onSelect={viewCheatsheet}
+            onSelect={browseOption}
             onChange={updateTerm}
             value={searchTerm}
+            fuseConfigs={{
+                threshold: 0.8,
+              }}
         />
     )
 }
