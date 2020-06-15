9131c1950686f78540edf77b0ed1b24450620401
Tomashiwa <wongqinliang@gmail.com>
2020-06-15 14:16:49 +0800

    Modified s3.config to try fix AWS error when deploy

--- config/s3.config.js
+++ config/s3.config.js
@@ -1,21 +1,37 @@
-const AWS = require('aws-sdk');
-const env = require('./s3.env.js');
+if (!process.env.NODE_ENV || process.env.NODE_ENV === 'development') {
+    const AWS = require('aws-sdk');
+    const env = require('./s3.env.js');
+
+    const s3Client = new AWS.S3({accessKeyId: env.AWS_ACCESS_KEY, secretAccessKey: env.AWS_SECRET_ACCESS_KEY, region: env.region})
+
+    const uploadParams = {
+        Bucket: env.Bucket, 
+        Key: '', // pass key
+        Body: null, // pass file body
+        ACL: 'public-read'
+    };
+     
+    const s3 = {};
+    s3.s3Client = s3Client;
+    s3.uploadParams = uploadParams;
+     
+    module.exports = s3;
+} else {
+    const AWS = require('aws-sdk');
+
+    const s3Client = new AWS.S3({accessKeyId: process.env.S3_KEY, secretAccessKey: process.env.S3_SECRET, region: process.env.S3_REGION});
+
+    const uploadParams = {
+        Bucket: env.Bucket, 
+        Key: '', // pass key
+        Body: null, // pass file body
+        ACL: 'public-read'
+    };
+     
+    const s3 = {};
+    s3.s3Client = s3Client;
+    s3.uploadParams = uploadParams;
+     
+    module.exports = s3;
+}
  
-const s3Client = new AWS.S3({
-    accessKeyId: env.AWS_ACCESS_KEY,
-    secretAccessKey: env.AWS_SECRET_ACCESS_KEY,
-    region : env.REGION
-});
- 
-const uploadParams = {
-    Bucket: env.Bucket, 
-    Key: '', // pass key
-    Body: null, // pass file body
-    ACL: 'public-read'
-};
- 
-const s3 = {};
-s3.s3Client = s3Client;
-s3.uploadParams = uploadParams;
- 
-module.exports = s3;
