5c2d6e59498501aade3efd11b48d51f3115d4da6
Tomashiwa <wongqinliang@gmail.com>
2020-06-19 17:04:39 +0800

    Basic functionality of Upload page

---
+++ client/src/components/UploadForm.js
@@ -0,0 +1,229 @@
+import React, { useState, useEffect, useRef, useContext } from 'react'
+import { invalidSymbols } from "../misc/InvalidSymbols.js";
+import axios from 'axios';
+import { Form, FormGroup, Label, Input, FormFeedback, FormText } from "reactstrap";
+import CreatableSelect from "react-select/creatable";
+import "./css/UploadForm.css"
+
+function UploadForm({form, setForm, setBlob}) {
+    const [file, setFile] = useState(null);
+    const [schools, setSchools] = useState([]);
+    const [modules, setModules] = useState([]);
+
+    const [schoolOptions, setSchoolOptions] = useState([]);
+    const [moduleOptions, setModuleOptions] = useState([]);
+
+    const [nameState, setNameState] = useState({valid: false, invalid: false});
+    const [schState, setSchState] = useState({isLoading: false, isDisabled: false, selected: null});
+    const [modState, setModState] = useState({isLoading: false, isDisabled: false, selected: null});
+
+    const hasInvalidSymbols = str => {
+        let isInvalid = false;
+        for(let symbol of invalidSymbols) {
+            if(str.includes(symbol)) {
+                isInvalid = true;
+                break;
+            }
+        }
+        return isInvalid;
+    }
+
+    const fetchSchs = callback => {
+        axios.get("/api/schools")
+            .then(res => {
+                setSchools(res.data);
+                callback();
+            })
+            .catch(err => {
+                console.log(`Fail to fetch Schools: ${err}`);
+            });
+    }
+
+    const fetchMods = callback => {
+        axios.get("/api/modules")
+            .then(res => {
+                setModules(res.data);
+                callback();
+            })
+            .catch(err => {
+                console.log(`Fail to fetch modules: ${err}`);
+            });
+    }
+
+    // Fetching schools and modules from server
+    useEffect(() => {
+        fetchSchs();
+        fetchMods();
+    }, []);
+
+    // Create options based on fetched schools and modules
+    useEffect(() => {
+        setSchoolOptions(schools.map(school => {return {label: school.name, value: school._id}}));
+        setModuleOptions(modules
+            .filter(module => module.school && schState.selected && module.school === schState.selected.value)
+            .map(module => {return {label: `${module.code} - ${module.name}`, value: module._id}}));
+    }, [schools, modules, schState.selected])
+
+    // Verify the name and save it to the form
+    useEffect(() => {
+        const nameInput = document.querySelector("#uploadform-input-name");
+
+        const checkName = e => {
+            const isValid = e.target.value.length && !hasInvalidSymbols(e.target.value);
+            const isInvalid = e.target.value.length && hasInvalidSymbols(e.target.value);
+
+            if(nameState.valid !== isValid || nameState.invalid !== isInvalid) {
+                setNameState({valid: isValid, invalid: isInvalid});
+            }
+        }
+
+        const changeName = e => setForm({...form, ...{name: e.target.value}});
+
+        nameInput.addEventListener("input", checkName);
+        nameInput.addEventListener("change", changeName);
+
+        return () => {
+            nameInput.removeEventListener("input", checkName);
+            nameInput.removeEventListener("change", changeName);
+        };
+    }, [form, nameState, setForm])
+
+    // Saving the description to the form
+    useEffect(() => {
+        const descInput = document.querySelector("#uploadform-input-desc");
+        const changeDesc = e => setForm({...form, ...{description: e.target.value}});
+        
+        descInput.addEventListener("change", changeDesc);
+
+        return () => descInput.removeEventListener("change", changeDesc);
+    }, [form, setForm, nameState]);
+
+    // Saving the isPublic to the form
+    useEffect(() => {
+        const isPublicInput = document.querySelector("#uploadform-input-public");
+        const changeIsPublic = e => setForm({...form, ...{isPublic: e.target.checked}});
+
+        isPublicInput.addEventListener("change", changeIsPublic);
+
+        return () => isPublicInput.removeEventListener("change", changeIsPublic);
+    }, [form, setForm, nameState]);
+
+    // Save image input as blob
+    useEffect(() => {
+        const fileInput = document.querySelector("#uploadform-input-file");
+        
+        const loadFile = e => {
+            const file = e.target.files[0];
+            setBlob(file);
+        }
+
+        fileInput.addEventListener("change", loadFile);
+        return () => fileInput.removeEventListener("change", loadFile)
+    }, [setBlob])
+
+    // Save a selected school to the form
+    const saveSchool = option => {
+        if(option) {
+            setSchState({...schState, ...{selected: {label: option.label, value: option.value}}});
+            setModState({...modState, ...{selected: null}})
+            setForm({...form, ...{school: option.value}})
+        }
+    }
+
+    // Save a selected module to the form
+    const saveModule = option => {
+        if(option) {
+            setModState({...modState, ...{selected: {label: option.label, value: option.value}}});
+            setForm({...form, ...{module: option.value}});
+        }
+    }
+
+    // Add new school to the backend
+    const addSchool = value => {
+        const newSchool = {name: value}
+
+        setSchState({...schState, ...{isDisabled: true, isLoading: true}});
+
+        axios.post("/api/schools", newSchool)
+            .then(res => {
+                fetchSchs(() => {
+                    setSchState({isDisabled: false, isLoading: false, selected: {label: value, value: value}});
+                    setForm({...form, ...{school: value}})
+                });
+            })
+            .catch(err => console.log(`Fail to add school: ${err}`));
+    }
+
+    // Add new module to the backend
+    const addModule = value => {
+        const newModule = {school: schState.selected.value, code: value, name: value}
+
+        setModState({...modState, ...{isDisabled: true, isLoading: true}});
+
+        axios.post("/api/modules", newModule)
+            .then(res => {
+                fetchMods(() => {
+                    setModState({isDisabled: false, isLoading: false, selected: {label: value, value: value}});
+                    setForm({...form, ...{module: value}})
+                });
+            })
+            .catch(err => console.log(`Fail to add mod: ${err}`));
+    }
+
+    return (
+        <Form id="form-upload">
+            <FormGroup>
+                <Label>File</Label>
+                <Input id="uploadform-input-file" type="file" accept="image/*"/>
+                <FormText>Choose an image file to be uploaded.</FormText>
+            </FormGroup>
+            <FormGroup>
+                <Label>Name</Label>
+                <Input id="uploadform-input-name" 
+                    valid={nameState.valid ? true : false} 
+                    invalid={nameState.invalid ? true : false}/>
+                <FormFeedback invalid="true">Name cannot contains an invalid symbol.</FormFeedback>
+                <FormText>Enter the name of your cheatsheet.</FormText>
+            </FormGroup>
+            <FormGroup>
+                <Label>School</Label>
+                <CreatableSelect 
+                    isClearable
+                    isDisabled={schState.isDisabled}
+                    isLoading={schState.isLoading}
+                    onChange={saveSchool}
+                    onCreateOption={addSchool}
+                    options={schoolOptions}
+                    value={schState.selected}
+                />
+                <FormText>School that your cheatsheet is for.</FormText>
+            </FormGroup>
+            <FormGroup>
+                <Label>Module</Label>
+                <CreatableSelect
+                    isClearable
+                    isDisabled={modState.isDisabled || schState.selected === null}
+                    isLoading={modState.isLoading}
+                    onChange={saveModule}
+                    onCreateOption={addModule}
+                    options={moduleOptions}
+                    value={modState.selected}
+                />
+                <FormText>Module that your cheatsheet is for.</FormText>
+            </FormGroup>
+            <FormGroup>
+                <Label>Description</Label>
+                <Input id="uploadform-input-desc" type="textarea"/>
+                <FormText>Information that may help readers understand your cheatsheet.</FormText>
+            </FormGroup>
+            <FormGroup check>
+                <Label check>
+                    <Input id="uploadform-input-public" type="checkbox"/> Share with public
+                </Label>
+            </FormGroup>
+
+        </Form>
+    )
+}
+
+export default UploadForm
--- client/src/pages/Upload.js
+++ client/src/pages/Upload.js
@@ -1,8 +1,189 @@
-import React from 'react'
+import React, {useState, useContext, useRef, useEffect} from 'react'
+import Stepper from 'react-stepper-horizontal'
+import {useHistory} from "react-router-dom"
+import UploadForm from "../components/UploadForm"
+import ImagePreviewer from '../components/ImagePreviewer';
+import {Container, Button} from 'reactstrap';
+import UserContext from '../context/UserContext';
+import axios from 'axios';
+import uuid from "uuid";
+import mongoose from "mongoose";
+
+import "./css/Upload.css";
+
+export const UPLOAD_STEP_FORM = 1;
+export const UPLOAD_STEP_PREVIEW = 2;
 
 function Upload() {
+    const {userData} = useContext(UserContext);
+    const [formStep, setFormStep] = useState(UPLOAD_STEP_FORM);
+    const [form, setForm] = useState({
+        url: "",
+        name: "",
+        school: "",
+        module: "",
+        description: "",
+        isPublic: false
+    });
+    const [nextEnabled, setNextEnabled] = useState(false);
+    const [sheetId, setSheetId] = useState(undefined);
+    const blobRef = useRef(null);
+
+    const history = useHistory();
+    const setBlob = blob => blobRef.current = blob;
+
+    // Navigation events that happened when Next button is pressed
+    useEffect(() => {
+        const upload = () => {
+            const formData = new FormData();
+            formData.append("file", blobRef.current, `${form.name}-${uuid.v4()}.png`);
+            
+            axios.post("/upload", formData)
+                .then(res => {
+                    saveToDb(res.data.data.Location);
+                    setForm({...form, ...{url: res.data.data.Location}});
+                })
+                .catch(err => console.log(err));
+        };
+
+        const saveToDb = url => {
+            const newCheatsheet = {
+                file: url,
+                user: mongoose.Types.ObjectId(userData.user.id),
+                name: form.name,
+                school: mongoose.Types.ObjectId(form.school),
+                module: mongoose.Types.ObjectId(form.module),
+                description: form.description,
+                datetime: Date.now(),
+                rating: 0,
+                comments: [],
+                isPublic: form.isPublic
+            }
+
+            axios.post("/api/cheatsheets/add", newCheatsheet)
+                .then(sheet => {
+                    setSheetId(sheet.data._id);
+                })
+                .catch(err => console.log(err));
+        }
+        
+
+        const hasStepCompleted = () => {
+            return true;
+        }
+
+        const endStep = () => {
+            if(formStep === UPLOAD_STEP_FORM) {
+                console.log("current step is FORM, will be uploading");
+                upload();
+            }
+        }
+
+        const nextStep = () => {
+            console.log("NEXT STEP");
+            const nextStep = formStep + 1 < 2
+                ? formStep + 1
+                : UPLOAD_STEP_PREVIEW;
+            setFormStep(nextStep);
+        }
+
+        const next = e => {
+            console.log("next clicked...");
+
+            if(hasStepCompleted()) {
+                console.log("current step completed, going to next step");
+                endStep();
+                nextStep();
+            }
+        };
+
+        const nextBtn = document.querySelector("#upload-btn-next");
+        if(nextBtn) {
+            nextBtn.addEventListener("click", next);
+        }
+
+        return () => {
+            if(nextBtn) {
+                nextBtn.removeEventListener("click", next);
+            }
+        }
+    }, [formStep, form, userData])
+
+
+    // Verify if user can proceed to next step and toggle the Next button
+    useEffect(() => {
+        if(formStep === UPLOAD_STEP_FORM) {
+            if(!nextEnabled && blobRef !== null && form.name.length > 0 && form.school.length > 0 && form.module.length > 0) {
+                setNextEnabled(true);
+            } else if(nextEnabled && (blobRef === null || form.name.length === 0 || form.school.length === 0 || form.module.length === 0)) {
+                setNextEnabled(false);
+            }
+        } else if (formStep === UPLOAD_STEP_PREVIEW) {
+            setNextEnabled(false);
+        }
+    }, [form.module, form.name, form.school, form.url, formStep, nextEnabled]);
+    
+    // Attach view sheet event on Finish button
+    useEffect(() => {
+        const finishButton = document.querySelector("#upload-btn-finish");
+        
+        const viewSheet = () => {
+            if(sheetId !== undefined) {
+                history.push(`/view/${sheetId}`);
+            }
+        }
+
+        if(formStep === UPLOAD_STEP_PREVIEW) {    
+            finishButton.addEventListener("click", viewSheet);
+        }
+
+        return () => {
+            if(formStep === UPLOAD_STEP_PREVIEW) {
+                finishButton.removeEventListener("click", viewSheet);
+            }
+        }
+    }, [formStep, sheetId, history])
+
+
     return (
         <div>
+            <Container id="upload-container">
+                <Stepper
+                    size={40}   
+                    activeColor="#505050"
+                    completeColor="#505050"
+                    activeTitleColor="#FFF"
+                    completeTitleColor="#FFF"
+                    steps={[{title: "Upload"}, {title: "Preview"}]}
+                    activeStep={formStep - 1}
+                />
+
+                <div id="upload-title-nav">
+                    {
+                        formStep === UPLOAD_STEP_FORM
+                            ? <h2>Upload your cheatsheet</h2>
+                        : formStep === UPLOAD_STEP_PREVIEW
+                            ? <h2>Preview</h2>
+                        : <div></div>
+                    }
+                    {
+                        formStep === UPLOAD_STEP_PREVIEW
+                            ? <Button id="upload-btn-finish" disabled={!(sheetId !== undefined)} outline color="light">
+                                Finish
+                            </Button>
+                            : <Button id="upload-btn-next" disabled={!nextEnabled} outline color="light">
+                                Next
+                              </Button>
+                    }
+                </div>
+                {
+                    formStep === UPLOAD_STEP_FORM
+                        ? <UploadForm form={form} setForm={setForm} setBlob={setBlob}/>
+                    : formStep === UPLOAD_STEP_PREVIEW
+                        ? <ImagePreviewer imageURL={form.url} />
+                    : <div></div>
+                }
+            </Container>
         </div>
     )
 }
