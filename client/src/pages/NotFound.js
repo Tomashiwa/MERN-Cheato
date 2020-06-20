3bbd9bb17af6dce24d6ddad5a862b081169b5617
Tomashiwa <wongqinliang@gmail.com>
2020-06-20 16:29:20 +0800

    Styling on NotFound page

--- client/src/pages/NotFound.js
+++ client/src/pages/NotFound.js
@@ -1,11 +1,28 @@
 import React from 'react'
+import { Container, Button, Card, CardHeader, CardBody, CardText } from "reactstrap";
+import { useHistory } from 'react-router-dom';
 
 function NotFound() {
+
+    const history = useHistory();
+
+    const goHome = () => {
+        history.push("/");
+    }
+
     return (
-        <div>
-            <h1>PAGE NOT FOUND</h1>
-        </div>
-    )
+        <Container id="notfound-container">
+            <Card>
+                <CardHeader tag="h3">Page not found</CardHeader>
+                <CardBody>
+                    <CardText>
+                        The page you trying to acccess does not exist. Please try again or return to home.
+                    </CardText>
+                    <Button onClick={goHome}>Back to Home</Button>
+                </CardBody>
+            </Card>
+        </Container> 
+)
 }
 
 export default NotFound
