966d1a7c34fca51455e1857de68160603d312636
Tomashiwa <wongqinliang@gmail.com>
2020-06-25 21:05:20 +0800

    Refactored Upload page
    Note: Convert all "addEventListener" usage to event handlers in elements/components

--- client/src/pages/Upload.js
+++ client/src/pages/Upload.js
@@ -32,85 +32,59 @@ function Upload() {
     const history = useHistory();
 
     // Navigation events that happened when Next button is pressed
-    useEffect(() => {
-        const upload = () => {
-            const formData = new FormData();
-            formData.append("file", blob, `${form.name}-${uuid.v4()}.png`);
-            
-            axios.post("/upload", formData)
-                .then(res => {
-                    saveToDb(res.data.data.Location);
-                    setForm({...form, ...{url: res.data.data.Location}});
-                })
-                .catch(err => console.log(err));
-        };
-
-        const saveToDb = url => {
-            const newCheatsheet = {
-                file: url,
-                user: userData.isLoaded && userData.token === undefined
-                    ? mongoose.Types.ObjectId(-1)
-                    : mongoose.Types.ObjectId(userData.user.id),
-                name: form.name,
-                school: mongoose.Types.ObjectId(form.school),
-                module: mongoose.Types.ObjectId(form.module),
-                description: form.description,
-                datetime: Date.now(),
-                rating: 0,
-                comments: [],
-                isPublic: form.isPublic,
-                isAnonymous: userData.isLoaded && userData.token === undefined
-            }
-
-            axios.post("/api/cheatsheets/add", newCheatsheet)
-                .then(sheet => {
-                    setSheetId(sheet.data._id);
-                })
-                .catch(err => console.log(err));
+    const saveToDb = url => {
+        const newSheet = {
+            file: url,
+            user: userData.isLoaded && userData.token === undefined
+                ? mongoose.Types.ObjectId(-1)
+                : mongoose.Types.ObjectId(userData.user.id),
+            name: form.name,
+            school: mongoose.Types.ObjectId(form.school),
+            module: mongoose.Types.ObjectId(form.module),
+            description: form.description,
+            datetime: Date.now(),
+            rating: 0,
+            comments: [],
+            isPublic: form.isPublic,
+            isAnonymous: userData.isLoaded && userData.token === undefined
         }
+
+        axios.post("/api/cheatsheets/add", newSheet)
+            .then(sheet => {
+                setSheetId(sheet.data._id);
+            })
+            .catch(err => console.log(err));
+    }
+
+    const upload = () => {
+        const formData = new FormData();
+        formData.append("file", blob, `${form.name}-${uuid.v4()}.png`);
         
+        axios.post("/upload", formData)
+            .then(res => {
+                saveToDb(res.data.data.Location);
+                setForm({...form, ...{url: res.data.data.Location}});
+            })
+            .catch(err => console.log(err));
+    };
 
-        const hasStepCompleted = () => {
-            return true;
+    const saveSheet = () => {
+        if(formStep === UPLOAD_STEP_FORM) {
+            upload();
         }
+    }
 
-        const endStep = () => {
-            if(formStep === UPLOAD_STEP_FORM) {
-                console.log("current step is FORM, will be uploading");
-                upload();
-            }
-        }
+    const openPreviewer = () => {
+        const nextStep = formStep + 1 < 2
+            ? formStep + 1
+            : UPLOAD_STEP_PREVIEW;
+        setFormStep(nextStep);
+    }
 
-        const nextStep = () => {
-            console.log("NEXT STEP");
-            const nextStep = formStep + 1 < 2
-                ? formStep + 1
-                : UPLOAD_STEP_PREVIEW;
-            setFormStep(nextStep);
-        }
-
-        const next = e => {
-            console.log("next clicked...");
-
-            if(hasStepCompleted()) {
-                console.log("current step completed, going to next step");
-                endStep();
-                nextStep();
-            }
-        };
-
-        const nextBtn = document.querySelector("#upload-btn-next");
-        if(nextBtn) {
-            nextBtn.addEventListener("click", next);
-        }
-
-        return () => {
-            if(nextBtn) {
-                nextBtn.removeEventListener("click", next);
-            }
-        }
-    }, [formStep, form, userData, blob])
-
+    const finishForm = e => {
+        saveSheet();
+        openPreviewer();
+    };
 
     // Verify if user can proceed to next step and toggle the Next button
     useEffect(() => {
@@ -125,28 +99,13 @@ function Upload() {
         }
     }, [form.module, form.name, form.school, form.url, formStep, nextEnabled, blob]);
     
-    // Attach view sheet event on Finish button
-    useEffect(() => {
-        const finishButton = document.querySelector("#upload-btn-finish");
-        
-        const viewSheet = () => {
-            if(sheetId !== undefined) {
-                history.push(`/view/${sheetId}`);
-                window.location.reload();
-            }
+    // Direct to the sheet's view page
+    const viewSheet = () => {
+        if(sheetId !== undefined) {
+            history.push(`/view/${sheetId}`);
+            window.location.reload();
         }
-
-        if(formStep === UPLOAD_STEP_PREVIEW) {    
-            finishButton.addEventListener("click", viewSheet);
-        }
-
-        return () => {
-            if(formStep === UPLOAD_STEP_PREVIEW) {
-                finishButton.removeEventListener("click", viewSheet);
-            }
-        }
-    }, [formStep, sheetId, history])
-
+    }
 
     return (
         <div>
@@ -171,10 +130,10 @@ function Upload() {
                     }
                     {
                         formStep === UPLOAD_STEP_PREVIEW
-                            ? <Button id="upload-btn-finish" disabled={!(sheetId !== undefined)} outline color="light">
+                            ? <Button id="upload-btn-finish" disabled={!(sheetId !== undefined)} outline color="light" onClick={viewSheet}>
                                 Finish
                             </Button>
-                            : <Button id="upload-btn-next" disabled={!nextEnabled} outline color="light">
+                            : <Button id="upload-btn-next" disabled={!nextEnabled} outline color="light" onClick={finishForm}>
                                 Next
                               </Button>
                     }
