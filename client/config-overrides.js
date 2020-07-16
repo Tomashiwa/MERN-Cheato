f1691aa42ed6959cff88fff26c8e1da85f350a01
Tomashiwa <wongqinliang@gmail.com>
2020-07-16 14:48:36 +0800

    Test push (Setup preloadWebpackPlugin)

--- client/config-overrides.js
+++ client/config-overrides.js
@@ -1,3 +1,14 @@
+// const HtmlWebpackPlugin = require("html-webpack-plugin");
+const PreloadWebpackPlugin = require('preload-webpack-plugin');
+
 module.exports = function override(config, env) {
+	config.plugins.push(
+		// new HtmlWebpackPlugin(),
+		new PreloadWebpackPlugin({
+			rel: "preload",
+			include: ["precache-manifest"]
+		})
+	);
+
 	return config;
 };
